8eb03723befbfff75cce7644e5c8955e00903c0e
mangisto14 <mangisto1410@gmail.com>
2026-01-18 22:55:33 +0200

    chenge a greet function massege

--- App.py
+++ App.py
@@ -2,7 +2,8 @@ import datetime
 
 def greet(name):
     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return f"Hello, {name}! Current time: {current_time}"
+    return f"👋 Hello, {name}! ⏰ Current time: {current_time}"
+
 
 
 if __name__ == "__main__":
